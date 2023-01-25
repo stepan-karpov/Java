9f3d39d95e1196fb6ccce2722b0a654e506a9cad
Stepan <stepan.karpov@list.ru>
2023-01-25 12:33:26 +0300

    SplayTree added

---
+++ SplayTree.java
@@ -0,0 +1,211 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SplayTree {
+  static public class Node {
+    String value;
+    String key;
+    Integer left_son = -1;
+    Integer right_son = -1;
+    Integer parent = -1;
+    public Node(String value, String key) {
+      this.value = value;
+      this.key = key;
+    }
+    public Node() {}
+  }
+
+  public Boolean Less(Node a, Node b) {
+    Integer res = a.value.compareTo(b.value);
+    if (res < 0) {
+      return true;
+    } else if (res == 0) {
+      if (a.key.compareTo(b.key) < 0) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+
+  public Boolean Equals(Node a, Node b) {
+    if (a.value.compareTo(b.value) != 0) {
+      return false;
+    }
+    return a.key.compareTo(b.key) == 0;
+  }
+
+  ArrayList<Node> tree = new ArrayList<Node>();
+
+  private void AssignParent(Integer subtree, Integer parent) {
+    if (subtree != -1) {
+      tree.get(subtree).parent = parent;
+    }
+  }
+
+  private void LeftZig(Integer p) {
+    Node cur_p = tree.get(p);
+    Integer x = cur_p.left_son;
+    Node cur_x = tree.get(x);
+    Integer A = cur_x.left_son;
+    Integer B = cur_x.right_son;
+    Integer C = cur_p.right_son;
+    Collections.swap(tree, p, x);
+    Integer temp = x; x = p; p = temp;
+    cur_x = tree.get(x); cur_p = tree.get(p);
+    cur_x.left_son = A;
+    cur_x.right_son = p;
+    cur_p.left_son = B;
+    cur_p.right_son = C;
+    Integer temp_p = cur_p.parent;
+    cur_p.parent = cur_x.parent;
+    cur_x.parent = temp_p;
+    AssignParent(A, x);
+    AssignParent(B, p);
+    AssignParent(C, p);
+  }
+
+  private void RightZig(Integer p) {
+    Node cur_p = tree.get(p);
+    Integer x = cur_p.right_son;
+    Node cur_x = tree.get(x);
+    Integer A = cur_p.left_son;
+    Integer B = cur_x.left_son;
+    Integer C = cur_x.right_son;
+    Collections.swap(tree, p, x);
+    Integer temp = x; x = p; p = temp;
+    cur_x = tree.get(x); cur_p = tree.get(p);
+    cur_x.left_son = p;
+    cur_x.right_son = C;
+    cur_p.left_son = A;
+    cur_p.right_son = B;
+    Integer temp_p = cur_p.parent;
+    cur_p.parent = cur_x.parent;
+    cur_x.parent = temp_p;
+    AssignParent(A, p);
+    AssignParent(B, p);
+    AssignParent(C, x);
+  }
+
+  private void LeftZigZig(Integer g) {
+    Integer p = tree.get(g).left_son;
+    LeftZig(p);
+    LeftZig(g);
+  }
+
+  private void RightZigZig(Integer g) {
+    Integer p = tree.get(g).right_son;
+    RightZig(p);
+    RightZig(g);
+  }
+
+  private void LeftZigZag(Integer g) {
+    Integer p = tree.get(g).left_son;
+    RightZig(p);
+    LeftZig(g);
+  }
+
+  private void RightZigZag(Integer g) {
+    Integer p = tree.get(g).right_son;
+    LeftZig(p);
+    RightZig(g);
+  }
+
+  private void Splay(Integer x) {
+    Integer p = tree.get(x).parent;
+    if (p == -1) {
+      return;
+    }
+    Integer g = tree.get(p).parent;
+    if (g == -1) {
+      Node cur_p = tree.get(p);
+      if (cur_p.left_son == x) {
+        LeftZig(p);
+      } else {
+        RightZig(p);
+      }
+    } else {
+      Node cur_g = tree.get(g);
+      Node cur_p = tree.get(p);
+      if (cur_g.left_son == p) {
+        if (cur_p.left_son == x) {
+          LeftZigZig(g);
+        } else {
+          LeftZigZag(g);
+        }
+      } else {
+        if (cur_p.left_son == x) {
+          RightZigZag(g);
+        } else {
+          RightZigZig(g);
+        }
+      }
+    }
+  }
+  
+  private void Add(Node value, Integer x) {
+    Node cur_x = tree.get(x);
+    if (Equals(cur_x, value)) {
+      return;
+    }
+    if (Less(value, cur_x)) {
+      if (cur_x.left_son == -1) {
+        tree.add(value);
+        cur_x.left_son = tree.size() - 1;
+        tree.get(tree.size() - 1).parent = x;
+        return;
+      }
+      Add(value, cur_x.left_son);
+    } else {
+      if (cur_x.right_son == -1) {
+        tree.add(value);
+        cur_x.right_son = tree.size() - 1;
+        tree.get(tree.size() - 1).parent = x;
+        return;
+      }
+      Add(value, cur_x.right_son);
+    }
+  }
+
+  public void Add(Node value) {
+    if (tree.isEmpty()) {
+      tree.add(value);
+      return;
+    }
+    Add(value, 0);
+  }
+
+  private String Find(String value, Integer x) {
+    Node cur_x = tree.get(x);
+    if (cur_x.value.compareTo(value) == 0) {
+      Splay(x);
+      return cur_x.key;
+    }
+    if (cur_x.value.compareTo(value) > 0) {
+      if (cur_x.left_son == -1) {
+        return "None";
+      }
+      String ans = Find(value, cur_x.left_son);
+      if (ans != "None") {
+        Splay(x);
+      }
+      return ans;
+    } else {
+      if (cur_x.right_son == -1) {
+        return "None";
+      }
+      String ans = Find(value, cur_x.right_son);
+      if (ans != "None") {
+        Splay(x);
+      }
+      return ans;
+    }
+  }
+
+  public String Find(String value) {
+    if (tree.isEmpty()) {
+      return "None";
+    }
+    return Find(value, 0);
+  }
+}
